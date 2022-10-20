30e9027117ca930fef6513b8f9ce31d883f09a48
Stapiciak <dominik.rostar@gmail.com>
2022-10-20 14:42:36 +0200

    DNS

--- DnsService.py
+++ DnsService.py
@@ -1,5 +1,21 @@
-from DnsHeaderModel import DnsHeaderModel
+import struct
+
+import DnsHeader
+from DnsQueryBodyModel import DnsQueryBodyModel
+
 
 class DnsServcice:
 
-    def generate_header(self, pa_dns_header_model:DnsHeaderModel):
+    def generate_header(self, pa_dns_header_model:DnsHeader):
+        return struct.pack("!2H", pa_dns_header_model.translation_id,
+                           pa_dns_header_model.flags,
+                           pa_dns_header_model.question,
+                           pa_dns_header_model.answer,
+                           pa_dns_header_model.authority,
+                           pa_dns_header_model.additional)
+
+    def generate_query_body(self, pa_dns_query_body_model:DnsQueryBodyModel):
+
+        qtypeb = struct.pack("!H", pa_dns_query_body_model.type)
+        qclassb = struct.pack("!H", pa_dns_query_body_model.qclass)
+        pass
