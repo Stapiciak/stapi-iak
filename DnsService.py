47c79e7bc0584fc727e14d4e06e451984990f3dc
Stapiciak <dominik.rostar@gmail.com>
2022-10-20 14:26:41 +0200

    DNS

---
+++ DnsService.py
@@ -0,0 +1,5 @@
+from DnsHeaderModel import DnsHeaderModel
+
+class DnsServcice:
+
+    def generate_header(self, pa_dns_header_model:DnsHeaderModel):
---
+++ Main.py
@@ -0,0 +1,3 @@
+from DnsQueryBodyModel import DnsQueryBodyModel
+from DnsHeaderModel import DnsHeaderModel
+question_text = input("Enter DNS name: ")
