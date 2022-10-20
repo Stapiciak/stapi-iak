89d3eacd232337969d7c94a5034dc84296f281a5
Stapiciak <dominik.rostar@gmail.com>
2022-10-20 14:52:03 +0200

    DNS

--- DnsService.py
+++ DnsService.py
@@ -14,8 +14,19 @@ class DnsServcice:
                            pa_dns_header_model.authority,
                            pa_dns_header_model.additional)
 
-    def generate_query_body(self, pa_dns_query_body_model:DnsQueryBodyModel):
+    def _format_domain_to_query_name(self, pa_domain):
+        domain_splited = pa_domain
+        qname_result = bytes()
+        for label in domain_splited:
+            print(label)
+            qname_result += struct.pack("!B", len(label))
+            qname_result += label.encode()
+            qname_result += struct.pack("!B", 0)
+            return qname_result
+    pass
 
+    def generate_query_body(self, pa_dns_query_body_model:DnsQueryBodyModel):
+        qnameb = self._format_domain_to_query_name(pa_dns_query_body_model.domain)
         qtypeb = struct.pack("!H", pa_dns_query_body_model.type)
         qclassb = struct.pack("!H", pa_dns_query_body_model.qclass)
         pass
--- Main.py
+++ Main.py
@@ -1,3 +1,2 @@
 from DnsQueryBodyModel import DnsQueryBodyModel
-from DnsHeaderModel import DnsHeaderModel
 question_text = input("Enter DNS name: ")
